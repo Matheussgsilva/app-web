a918ae4e51b94a51b583984e36d8a14006286830
Matheussgsilva <81831589+Matheussgsilva@users.noreply.github.com>
2022-10-01 15:35:23 -0300

    Fix the imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DetailsComponent } from './pages/details/details.component';
 import { LoginComponent } from './pages/login/login.component';
 
 import { AuthGuard } from './guards/auth.guard';
-import { LayoutComponent } from 'src/app/pages/layout/layout.component';
+import { LayoutComponent } from './pages/layout/layout.component';
 
 
 const routes: Routes = [
