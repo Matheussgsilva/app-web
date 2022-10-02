b2dce4913429541ee85a0923a35f258aafc61d38
Matheussgsilva <81831589+Matheussgsilva@users.noreply.github.com>
2022-10-02 19:12:32 -0300

    Create the register form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { RecommendationModel } from '../../models/recommendation.model';
+import { dataset } from '../../data/recommendations';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html'
 })
 
-export class RegisterComponent {
-  
+export class RegisterComponent implements OnInit{
+  public recommendations: RecommendationModel[] = dataset;
+  public kinds: string[] = [];
+
+  ngOnInit(): void {
+    dataset.forEach((item) => {
+      if (!this.kinds.includes(item.kind)) {
+        this.kinds.push(item.kind);
+      }
+    });
+  }
 }
