795848eb227d20c593ceee8f7dee8bcc52400d43
Matheussgsilva <81831589+Matheussgsilva@users.noreply.github.com>
2022-10-02 19:43:28 -0300

    Update the form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -12,6 +12,11 @@ export class RegisterComponent implements OnInit{
   public recommendations: RecommendationModel[] = dataset;
   public kinds: string[] = [];
 
+  public title: string = '';
+  public kind: string = '';
+  public description: string = '';
+  public imageURL: string = '';
+
   ngOnInit(): void {
     dataset.forEach((item) => {
       if (!this.kinds.includes(item.kind)) {
