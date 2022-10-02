26656c193eacb9868cddcef8e0f03b3c9d966fd7
Matheussgsilva <81831589+Matheussgsilva@users.noreply.github.com>
2022-10-02 17:46:26 -0300

    Create page register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,15 @@ import { LoginComponent } from './pages/login/login.component';
 
 import { AuthGuard } from './guards/auth.guard';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: LayoutComponent, canActivate: [ AuthGuard ], children: [
     { path: '', component: HomeComponent },
-    { path: ':id/detalhes', component: DetailsComponent }
+    { path: ':id/detalhes', component: DetailsComponent },
+    { path: 'register', component: RegisterComponent }
   ]},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,21 @@ import { AppComponent } from './app.component';
 
 import { HomeComponent } from './pages/home/home.component';
 import { DetailsComponent } from './pages/details/details.component';
-
-import { RecommendationItemComponent } from './components/recommendation-item/recommendation-item.component';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+import { RecommendationItemComponent } from './components/recommendation-item/recommendation-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     DetailsComponent,
-    RecommendationItemComponent,
     LoginComponent,
-    LayoutComponent
+    LayoutComponent,
+    RegisterComponent,
+    RecommendationItemComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
