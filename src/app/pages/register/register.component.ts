090dadb9fa9a9d7bc8756e9de253c9c67b596f91
Matheussgsilva <81831589+Matheussgsilva@users.noreply.github.com>
2022-10-05 20:20:40 -0300

    Add httpClient in register component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
-import { RecommendationModel } from '../../models/recommendation.model';
+import { HttpClient } from '@angular/common/http';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { CategoryModel } from '../../models/category.model';
 
 @Component({
   selector: 'app-register',
@@ -9,15 +11,48 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 
 export class RegisterComponent implements OnInit{
-  public recommendations: RecommendationModel[] = dataset;
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
-  public kinds: string[] = []
+  public catergories: CategoryModel[] = [];
 
-  public form: FormGroup = new FormGroup( {
-    name: new FormControl(''),
-    imageURL: new FormControl(''),
-    category: new FormControl(''),
-    description: new FormControl(''),
+  public form: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    image_url: new FormControl(''),
+    category_id: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
   })
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  public save(): void {
+    const url = 'https://jp-recommendations-api.herokuapp.com/recommendations';
+
+    if (this.form.valid) {
+      this.httpClient
+        .post(url, this.form.value)
+        .toPromise()
+        .then((_) => {
+          this.router.navigateByUrl('');
+        })
+        .catch((resp) => {
+          alert(resp.error.error);
+        });
+    } else {
+      alert('Favor preencher o formulário corretamente')
+    }
+  }
+
+  private loadCategories(): void {
+    const url = 'https://jp-recommendations-api.herokuapp.com/categories';
+
+    this.httpClient
+      .get<CategoryModel[]>(url)
+      .toPromise()
+      .then((data) => {
+        console.log(data)
+        this.catergories = data;
+      })
+  }
+}
