70cc1ee3430da26dbace75fd14619c4b6c6626c6
Matheussgsilva <81831589+matheussgsilva@users.noreply.github.com>
2022-10-25 11:08:28 -0300

    Update page login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,47 +1,22 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { HttpClient} from '@angular/common/http';
-import { Router } from '@angular/router';
-
 import { AuthService } from '../../services/auth.service';
-
-import { UserModel } from '../../models/user.model';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  constructor(
-    private authService: AuthService,
-    private httpClient: HttpClient,
-    private router: Router
-    ){}
-
-  public email: string = '';
-  public password: string = '';
-
-  public login(): void {
-    const url = 'https://jp-recommendations-api.herokuapp.com/sessions';
-
-    if (this.form.valid) {
-      this.httpClient
-        .post<UserModel>(url, this.form.value)
-        .toPromise()
-        .then((_) => {
-          this.router.navigateByUrl('');
-        })
-        .catch((resp) => {
-          alert(resp.error.error);
-        });
-    } else {
-      alert('Favor preencher o formulário corretamente')
-    }
-  }
+  constructor(private authService: AuthService) {}
 
   public form: FormGroup = new FormGroup({
-    password: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required]),
-  })
+    password: new FormControl('', [Validators.required]),
+  });
 
+  public login(): void {
+    if (this.form.valid) {
+      this.authService.login(this.form.value.email);
+    }
+  }
 }
