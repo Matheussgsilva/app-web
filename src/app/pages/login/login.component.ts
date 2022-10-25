0115c96336a66fe3e49b89c3f864afe87714f4b4
Matheussgsilva <81831589+matheussgsilva@users.noreply.github.com>
2022-10-25 11:25:35 -0300

    Add apiService in page Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,22 +1,36 @@
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
+
 import { AuthService } from '../../services/auth.service';
+import { ApiService } from '../../services/api.service';
+
+import { UserModel } from '../../models/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private apiService: ApiService
+  ) {}
 
   public form: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
+    email: new FormControl('joao.scarpa@designa.com', [Validators.required]),
+    password: new FormControl('123456', [Validators.required]),
   });
 
   public login(): void {
     if (this.form.valid) {
-      this.authService.login(this.form.value.email);
+      this.apiService
+        .post<UserModel>('/sessions', this.form.value)
+        .then((data) => {
+          this.authService.login(data);
+        })
+        .catch((response) => {
+          alert(response.error.error);
+        });
     }
   }
 }
